ab284f23f29a8b7d223319eb4c143fed923b8605
Niltonsf <nilton.schumacher.filho@gmail.com>
2024-06-08 15:25:48 -0300

    feat: creates room info

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -4,20 +4,15 @@ import React from 'react'
 import Info from './Info'
 import Participants from './Participants'
 import Toolbar from './Toolbar'
-import { useSelf } from '@/liveblocks.config'
 
 interface CanvasProps {
   boardId: string
 }
 
 const Canvas = ({ boardId }: CanvasProps) => {
-  const info = useSelf((me) => me.info)
-
-  console.log('canvas: ', boardId, info)
-
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <Toolbar />
     </main>
--- app/board/[boardId]/_components/Info.tsx
+++ app/board/[boardId]/_components/Info.tsx
@@ -1,9 +1,78 @@
+'use client'
+
+import Actions from '@/components/Actions'
+import { Button } from '@/components/Ui/Button'
+import Hint from '@/components/Ui/Hint'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { cn } from '@/lib/utils'
+import { useRenameModal } from '@/store/useRenameModal'
+import { useQuery } from 'convex/react'
+import { Menu } from 'lucide-react'
+import { Poppins } from 'next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-const Info = () => {
+interface InfoProps {
+  boardId: string
+}
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600'],
+})
+
+const TabSeparator = () => {
+  return <div className="text-neutral-300 px-1.5">|</div>
+}
+
+const Info = ({ boardId }: InfoProps) => {
+  const { onOpen } = useRenameModal()
+
+  const data = useQuery(api.board.get, {
+    id: boardId as Id<'boards'>,
+  })
+
+  if (!data) return <Info.Skeleton />
+
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      TODO: Information about the board
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild className="px-2" variant={'board'}>
+          <Link href={'/'}>
+            <Image src={'/logo.svg'} alt="board logo" height={40} width={40} />
+            <span
+              className={cn(
+                'font-semibold text-xl ml-2 text-black',
+                font.className,
+              )}
+            >
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          variant={'board'}
+          className="text-base font-normal px-2"
+          onClick={() => onOpen(data._id, data.title)}
+        >
+          {data.title}
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Actions id={data._id} title={data.title} side="bottom" sideOffset={10}>
+        <div>
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button size="icon" variant={'board'}>
+              <Menu />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   )
 }
