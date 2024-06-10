2dbd7b7f8af4bb5be08f915085f32a4be7497947
Niltonsf <nilton.schumacher.filho@gmail.com>
2024-06-10 18:12:22 -0300

    feat: creates canvas states for toolbar

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -1,20 +1,37 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import Info from './Info'
 import Participants from './Participants'
 import Toolbar from './Toolbar'
+import { CanvasMode, CanvasState } from '@/types/canvas'
+import { useHistory, useCanUndo, useCanRedo } from '@/liveblocks.config'
 
 interface CanvasProps {
   boardId: string
 }
 
 const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  })
+
+  const history = useHistory()
+  const canUndo = useCanUndo()
+  const canRedo = useCanRedo()
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canUndo={canRedo}
+        canRedo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   )
 }
